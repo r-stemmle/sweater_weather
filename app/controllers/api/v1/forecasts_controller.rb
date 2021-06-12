dbfb50945cb85cee8cd7bb89f1d301eae1805aa7
r-stemmle <ryan.stemmle@gmail.com>
2021-06-12 11:45:25 -0400

    Add api keys via figaro..add to .gitignore, hit maquest api for latnlng

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
     #dream driving ...
-    conn = Faraday.new('https://mapquest')
-    response = conn.get("/api/mapquest/loc=#{params[:location]}")
-    lat_n_long = JSON.parse(response.body, symbolize_names: true)
+    conn = Faraday.new('http://www.mapquestapi.com')
+    response = conn.get("/geocoding/v1/address?key=#{ENV['map_api_key']}&location=#{params[:location]}")
+    lat_n_lng = JSON.parse(response.body, symbolize_names: true)[:results].first[:locations].first[:latLng]
+    require "pry"; binding.pry
     conn = Faraday.new('https://weather')
     response = conn.get("/api/weather?loc=#{'latNlong'}")
     forecast_info = JSON.parse(response.body, symbolize_names: true)
