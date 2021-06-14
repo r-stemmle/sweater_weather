4252de8b8f1d70dfc50cbdd32d01698fda7b7802
r-stemmle <ryan.stemmle@gmail.com>
2021-06-14 07:11:11 -0400

    Build sessions controller with tdd

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,22 @@
 class Api::V1::SessionsController < ApplicationController
+  wrap_parameters User
+  before_action :set_user
+
   def create
+    if @user.try(:authenticate, user_params[:password])
+      render json: UsersSerializer.new(@user), status: 200
+    else
+      render json: {errors: ["incorrect password"]}, status: 404
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(email: user_params[:email])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
---
+++ spec/requests/sessions_create_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'post /api/v1/sessions', type: :request do
+  let(:valid_headers) {
+    Hash["Content-Type", "application/json", "Accept", "application/json"]
+  }
+  let(:valid_body) {
+    { user:
+      {
+        email: "testman@themail.com",
+        password: 'runner1234',
+      }
+    }
+  }
+
+  describe 'happy path' do
+    it "creates a user session and returns their api key" do
+      user = User.create!(email: "testman@themail.com", password: 'runner1234', api_key: 'p+K71asjfKVCw15UuzaKNpx2E80++Z4H')
+      post '/api/v1/sessions', headers: valid_headers, params: valid_body, as: :json
+      expect(response.status).to eq(200)
+      body = JSON.parse(response.body, symbolize_names: true)
+      expect(body).to be_a Hash
+      expect(body[:data]).to be_a Hash
+      expect(body[:data].keys).to include(:type, :id, :attributes)
+      expect(body[:data][:type]).to be_a String
+      expect(body[:data][:id]).to be_a String
+      expect(body[:data][:attributes]).to be_a Hash
+      expect(body[:data][:attributes].keys).to include(:email, :api_key)
+      expect(body[:data][:attributes][:email]).to be_a String
+      expect(body[:data][:attributes][:api_key]).to be_a String
+    end
+  end
+end
